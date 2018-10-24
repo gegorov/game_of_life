5dcf2337691e05ec24e57dc24b9af8dc15a0c149
gegorov <gegorov@yahoo.com>
2018-10-24 03:35:17 +0300

    refactor: changed size of the field

--- gol.js
+++ gol.js
@@ -1,12 +1,12 @@
 const readline = require('readline');
 const Game = require('./game');
 
-const [,, size = 36, speed = 1000] = process.argv;
+const [,, size = 24, speed = 500] = process.argv;
 
 const game = new Game(size);
 
 setInterval(() => {
-  let output = game.play();
+  const output = game.play();
 
   readline.cursorTo(process.stdout, 0, 0);
   readline.clearScreenDown(process.stdout);
